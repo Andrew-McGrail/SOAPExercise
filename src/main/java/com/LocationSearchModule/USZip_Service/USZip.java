3cb42dba0523df6dfebf476f69248d1003435f90
Andrew McGrail <andrew.mcgrail@orasi.com>
2018-02-19 16:59:01 -0500

    one file difference

---
+++ src/main/java/com/LocationSearchModule/USZip_Service/USZip.java
@@ -0,0 +1,12 @@
+package com.LocationSearchModule.USZip_Service;
+
+import com.orasi.api.soapServices.core.SoapService;
+
+public class USZip extends SoapService{
+	
+	public USZip() {
+		setServiceName("UsZipService");
+		setServiceURL("http://www.webservicex.net/uszip.asmx?wsdl");
+	}
+
+}
